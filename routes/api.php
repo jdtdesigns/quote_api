7cbfca70b946d771e1bc40cca9711f79dab7e4b8
jd <jt7903@gmail.com>
2017-05-24 17:47:03 -0400

    changed api url to quotes

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/quote', 'QuoteController@get');
+Route::get('/quotes', 'QuoteController@get');
