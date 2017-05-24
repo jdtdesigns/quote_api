c339859f42a757e50c435e404abe3b52d58899df
jd <jt7903@gmail.com>
2017-05-24 17:33:08 -0400

    initial

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Quote;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+    public function get()
+    {
+    	$type = request()->input('type');
+    	$amount = request()->input('amount');
+    	$id = request()->input('id');
+    	$cb = request()->input('callback');
+    	$first = !$amount && !$id && $type != 'rand';
+    	
+    	if ( $amount && $id )
+    		return response()->json(['error' => 'Cannot use ID param with Amount']);
+
+    	if ( $type && $id )
+    		return response()->json(['error' => 'Cannot use ID param with Type']);
+    	
+
+    	if ( $amount && !$type )
+    		$quotes = Quote::limit(intval($amount));
+
+    	if ( $amount && $type )    	
+    		$quotes = Quote::inRandomOrder()->limit(intval($amount));
+    	
+    	if ( $type == 'rand' && !$amount)
+    		$quotes = Quote::inRandomOrder()->first();
+
+    	if ( $id )
+    		$quotes = Quote::find(intval($id));
+
+			if ( $cb )
+				$quotes->withCallback($cb);
+
+			if ( $first ) $quotes = Quote::first();
+			return $type == 'rand' || $first ? $quotes : $quotes->get();    	
+    }
+}
